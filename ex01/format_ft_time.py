ef4146b3de1d37438b9d3fb4d5acbd7198601574
eth1711 <ethanlimwl@gmail.com>
2023-09-21 17:03:45 +0800

    ex02

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -1,5 +1,10 @@
 import datetime 
+import time
 
-x = datetime.datetime.now()
+x = "Seconds since January 1, 1970: {:,.4f} or {:.2e}"
+time = time.time()
+txt = datetime.datetime.now()
 
-print()
+print(x.format(time, time))
+
+print(txt.strftime("%b %d %Y"))
---
+++ ex02/find_ft_type.py
@@ -0,0 +1,3 @@
+def all_thing_is_obj(object: any) -> int:
+	if object == :
+		print((str.capitalize(type(object).__name__)), ":", type(object))
