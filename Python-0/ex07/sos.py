77fce1cd606a250f365335f6404693f0ee45cfd6
Ethan's Laptop <ethanlimwl@gmail.com>
2023-10-03 03:17:33 +0800

    done ex07

--- Python-0/ex07/sos.py
+++ Python-0/ex07/sos.py
@@ -2,6 +2,9 @@ import sys
 
 
 def main():
+    text = sys.argv
+    assert len(text) == 2, "arguments invalid"
+    assert text[1].isalnum(), "invalid string"
     morse = {" ": "/",
              "A": ".- ", 'B': "-...", 'C': "-.-.", 'D': "-..", 'E': '.',
              'F': '..-.', 'G': '--.', 'H': '....', 'I': '..',
@@ -11,4 +14,7 @@ def main():
              'V': '..-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
              'Z': '--..', '1': '.----', '2': '..---', '3': '...--',
              '4': '....-', '5': '.....', '6': '-....', '7': '--...',
-             '8': '---..', '9': '----.', '0': '-----'}
+             '8': '---..', '9': '----.', '0': '-----'}   
+    morse_list = [morse[char.upper()] for char in text[1] if char.upper() in morse]
+    morse_string = " ".join(morse_list)
+    print(morse_string)
