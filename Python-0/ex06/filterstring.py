dbe8aff9da6393177a08fdc4a176416ce3ed9cb2
eth1711 <ethanlimwl@gmail.com>
2023-10-02 20:31:23 +0800

    finished ex07

---
+++ Python-0/ex06/filterstring.py
@@ -0,0 +1,28 @@
+import sys
+import string
+from ft_filter import ft_filter
+
+def check_arg(text):
+    """checks for punctuatuin and whether it is printable.
+if all arguments valid, it returns True."""
+    for x in text:
+        if string.punctuation.find(x) != -1:
+            return False
+    if text.isprintable() is False:
+        return False
+    return True
+
+        
+
+def main():
+    assert len(sys.argv) == 3, "the arguments are bad"
+    text = sys.argv[1]
+    num = sys.argv[2]
+    assert sys.argv[2].isdigit(), "arguments 2 not valid"
+    assert check_arg(sys.argv[1]), "arguments 1 not valid"
+    print(ft_filter(lambda word : len(word) > int(num), text.split()))
+
+
+if __name__== "__main__":
+    main()    
+   
---
+++ Python-0/ex06/ft_filter.py
@@ -0,0 +1,10 @@
+def ft_filter(func, iter):
+    """function that basically filters an item. func can contain soemthing or be None.    
+Funtion should return an iterator of those items for which function(item) is true.
+if function is None, return the items that are true
+This function uses list comprehension, which needs to be used to fulfill part2 """
+    if func is None:
+        n_iter = [x for x in iter if x]
+    else:
+        n_iter = [x for x in iter if func(x)]
+    return (n_iter)
